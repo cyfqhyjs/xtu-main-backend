c2c7bd1ba564ccb1ba3fbe4587aee34851c31cde
YCYF <you@example.com>
2025-04-21 21:20:25 +0800

    Initial commit: Add Spring Boot backend structure

---
+++ src/main/java/com/xtuonline/xtu_backend/XtuBackendApplication.java
@@ -0,0 +1,13 @@
+package com.xtuonline.xtu_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XtuBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XtuBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xtuonline/xtu_backend/XtuBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xtuonline.xtu_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XtuBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
